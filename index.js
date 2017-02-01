c8f2ac0c00a138540c9eb500caf7504b72783a58
Andrej Lednicky <andrej@Andrejs-MacBook-Pro.local>
2017-02-01 17:50:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,115 @@
+/**
+ * Created by andrej on 1/23/17.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+var room = {
+    id : 0
+};
+var usedTokens = {
+    "k5sd4dfdg": 1,
+    "sdfsdf": 1,
+}
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('join', function (data) {
+        console.log("token: "+data["token"])
+
+        socket.join(data["token"]);
+        room.id = data["token"];
+
+        if(data["isController"]){
+            var status = 1;
+            if(usedTokens[data["token"]] == 1){
+                var obj ={
+
+                };
+                io.to(room.id).emit('update', obj);
+            }else{
+                status = "0"
+            }
+            var obj = {
+                status: status
+            };
+            io.to(room.id).emit('existRoom', obj);
+        }
+
+
+
+    });
+    socket.on('existRoom', function (roomId) {
+
+        console.log("exist room id:"+io.sockets.adapter.sids[socket.id][roomId])
+    });
+    socket.on('update', function(msg){
+        var obj ={
+
+        };
+        console.log("update")
+        io.to(room.id).emit('update', obj);
+    });
+    socket.on('setUpdate', function(obj){
+        console.log("setupdate")
+        io.to(room.id).emit('setUpdate', obj);
+    });
+    socket.on('displayTime', function(msg){
+        var obj ={
+            status: msg
+        };
+
+        io.to(room.id).emit('displayTime', obj);
+    });
+    socket.on('displayDate', function(msg){
+        var obj ={
+            status: msg
+        };
+
+        io.to(room.id).emit('displayDate', obj);
+    });
+
+    socket.on('displayRssFeed', function(url){
+        var obj ={
+            status: url
+        };
+
+        io.to(room.id).emit('displayRssFeed', obj);
+    });
+    socket.on('display24Format', function(msg){
+        var obj ={
+            status: msg
+        };
+
+        io.to(room.id).emit('display24Format', obj);
+    });
+    socket.on('rssFeedUrl', function(url){
+        console.log('rss feed url: ' + url);
+        var obj ={
+            url: url
+        };
+
+        io.to(room.id).emit('rssFeedUrl', obj);
+    });
+    socket.on('setDateFormatType', function(type){
+        var obj ={
+            type: type
+        };
+
+        io.to(room.id).emit('setDateFormatType', obj);
+    });
+
+});
+
+
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
